c0ca00d0b86dff6af3e53ba2d583a53d5832eb87
JaissonV <jaissonvalbuena29@outlook.com>
2023-05-15 22:32:40 -0500

    first commit in this repository to the front code

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import './css/App.css'
+import Login from './pages/login.jsx'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/navComponents/navBar'
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <button className="bg-blue-500 sm:bg-orange-400 lg:bg-green-400 hover:bg-green -700 font-bold text-white py-2 px-4 rounded">
+                Mybutton
+              </button>
+              <h1>1</h1>
+              <h2>2</h2>
+              <h3>3</h3>
+              <h4>4</h4>
+              <h5>5</h5>
+              <h6>6</h6>
+              <div className='m-10 mt-20 border p-2 pr-12'>
+                <p className='text-xl md:text-xs'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Eveniet impedit accusantium quas inventore placeat perferendis
+                  possimus pariatur excepturi optio, aperiam quos accusamus
+                  ipsam quod sed suscipit aspernatur eaque ab consequatur?
+                </p>
+              </div>
+            </>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<></>} />
+        <Route
+          path="/menu"
+          element={
+            <>
+              <NavBar></NavBar>
+            </>
+          }
+        />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navComponents/navBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../css/navBar.css'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>Navbar</h1>
+    </nav>
+  )
+}
+
+export default NavBar
