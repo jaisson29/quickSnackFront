643e1b485a3ebe0351a9c036c66c973b04e06489
JaissonV <jaissonvalbuena29@outlook.com>
2023-06-20 12:03:22 -0500

    probando funcionamiento de la api desde el backend/testing the api it's working form the back called form the front

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import './css/App.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Login from './pages/login.jsx'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/navComponents/navBar'
 
 function App() {
+  const url = 'http://localhost:5000'
+  const [datum, setDatum] = useState({
+    value: '',
+    value2: '',
+  })
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      const { value, value2 } = response.data // Accede a la propiedad data de la respuesta
+      setDatum({
+        value: value,
+        value2: value2,
+      })
+    })
+  }, [])
+  console.log(datum);
   return (
-    <div className="container mx-auto">
+    <div className="container">
       <Routes>
         <Route
           path="/"
           element={
             <>
-              <button className="bg-blue-500 sm:bg-orange-400 lg:bg-green-400 hover:bg-green -700 font-bold text-white py-2 px-4 rounded">
+              <h1>{datum.value} {datum.value2}</h1>
+              <button>
+                {' '}
+                {/* bg-blue-500 sm:bg-orange-400 lg:bg-green-400 hover:bg-green -700 font-bold text-white py-2 px-4 rounded */}
                 Mybutton
               </button>
               <h1>1</h1>
@@ -20,8 +40,8 @@ function App() {
               <h4>4</h4>
               <h5>5</h5>
               <h6>6</h6>
-              <div className='m-10 mt-20 border p-2 pr-12'>
-                <p className='text-xl md:text-xs'>
+              <div className="m-10 mt-20 border p-2 pr-12">
+                <p className="text-xl md:text-xs">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Eveniet impedit accusantium quas inventore placeat perferendis
                   possimus pariatur excepturi optio, aperiam quos accusamus
