3b8e8345c22fa1b214e3936fde520ad16b43496f
JaissonV <jaissonvalbuena29@outlook.com>
2023-08-18 12:15:03 -0500

    few changes into the design

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from './pages/login/Login.jsx'
 import Inicio from './pages/inicio/Inicio.jsx'
 import Registro from './pages/registro/Registro.jsx'
 import PageNotFound from './components/PageNotFound/404.jsx'
+import Home from './pages/home/Home.jsx'
 import Menu from './pages/menu/Menu.jsx'
 import { AuthProvider } from './components/Auth/Autenticacion.jsx'
 import { ProtectedRoute } from './components/ProtectedRoute.jsx'
@@ -12,16 +13,12 @@ function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/" exact element={<Inicio />} />
+        <Route path="/" index exact element={<Inicio />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/menu" element={<ProtectedRoute component={<Menu />} />} />
         <Route path="/registro" component={<Registro />} />
+        <Route path="/home" element={<ProtectedRoute component={<Home />} />} />
+        <Route path="/menu" element={<ProtectedRoute component={<Menu />} />} />
         <Route path="*" element={<PageNotFound />} />
-        <Route>
-          <ProtectedRoute>
-            <Login />
-          </ProtectedRoute>
-        </Route>
       </Routes>
     </AuthProvider>
   )
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,22 +1,18 @@
 import Logo from '../../assets/QSLogo.svg'
-import NavBar from '../../components/navbar/navBar'
 import './login.css'
-
 
 function Log() {
   return (
     <>
-
-      <NavBar page="login" />
-      <div className='loginContainer'>
-        <h1 className='text-red'>W</h1>
+      <main className="w-full h-full grid place-content-center">
+        <h1 className="text-red self-center">W</h1>
         <h2>W</h2>
         <h3>W</h3>
         <h4>W</h4>
         <h5>W</h5>
         <h6>W</h6>
-        <img src={Logo} alt=''></img>
-      </div>
+        <img src={Logo} alt=""></img>
+      </main>
     </>
   )
 }
