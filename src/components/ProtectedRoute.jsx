3042e6e0a40c19d43f8c8e32c760f8e3558c320a
AprendizCDA <AprendizCDA@CHIDPRCDGFD027.FormacionSena.RED>
2023-09-22 14:38:25 -0500

    Hola, mi ramita

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -8,11 +8,7 @@ export const ProtectedRoute = ({ children }) => {
   return authToken ? (
     <>
       <NavBar />
-<<<<<<< HEAD
-      <section className='absolute right-0 z-0 float-right h-full px-3 mx-1 w-21/24 md:w-22/24'>
-=======
       <section className='absolute right-0 z-0 float-right min-h-full px-3 mx-1 w-20/24 md:w-22/24'>
->>>>>>> 71fab051ab21a964f7f0520aa802f8963085288e
         <Header></Header>
         <main className='w-full h-full md:w-full'>{children}</main>
       </section>
