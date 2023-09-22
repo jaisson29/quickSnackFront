b3ebf8dd9ae04f3b18f53ce3993c847c13572167
AprendizCDA <AprendizCDA@CHIDPRCDGFD029.FormacionSena.RED>
2023-09-22 16:47:57 -0500

    vista de categoria

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Historial from './pages/historial/Historial.jsx'
 import { AuthProvider } from './components/Auth/Autenticacion.jsx'
 import { ProtectedRoute } from './components/ProtectedRoute.jsx'
 import Productos from './pages/productos/Productos.jsx'
+import Categoria from './pages/categoria/Categoria.jsx'
 
 function App() {
   return (
@@ -30,6 +31,10 @@ function App() {
         <Route
           path="/historial"
           element={<ProtectedRoute children={<Historial nom={'hola'} />} />}
+        />
+        <Route
+          path="/categorias"
+          element={<ProtectedRoute children={<Categoria />} />}
         />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
