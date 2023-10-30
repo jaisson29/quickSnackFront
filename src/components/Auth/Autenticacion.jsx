b32536afa73ecae623a7a248656a750ffb2cb691
Jaisson29 <jaissonvalbuena29@outlook.com>
2023-10-30 12:09:56 -0500

    adding a nre case into the reducer of the auth context

--- src/components/Auth/Autenticacion.jsx
+++ src/components/Auth/Autenticacion.jsx
@@ -12,24 +12,41 @@ export function AuthProvider({ children }) {
 	const [user, setUser] = useState(JSON.parse(sessionStorage.getItem('user')))
 	const [isAuth, setIsAuth] = useState(false)
 	const [balance, setBalance] = useState(0)
-	const [cartItems, setCartItems] = useState([20])
 	const urlApi = 'http://localhost:5000'
+
+	const instance = axios.create({
+		baseURL: urlApi,
+	})
+
+	instance.interceptors.response.use(
+		(response) => {
+			// Aquí puedes realizar acciones antes de que la respuesta sea devuelta
+			return response
+		},
+		(error) => {
+			// Cuando el servidor devuelva una peticion fallida con el codigo 401(No autroizado) cerrara la sesión
+			if (error.response.status === 401) {
+				logout() // Ejecución de logout() para limpieza de variables de sesión
+			}
+			return Promise.reject(error)
+		},
+	)
 
 	const login = async (token) => {
 		setAuthToken(token)
-		axios
+		instance
 			.get(`${urlApi}/api/login/verify`, {
 				headers: {
-					//Inicializa el header la el http
-					Authorization: `Bearer ${token}`, // Agrega el token al encabezado "Authorization"
+					//Inicializa el header de la paetición
+					Authorization: `Bearer ${token}`, // Agrega el token al encabezado "Authorization" para el envio del token por Bearer
 				},
 			})
-			.then((respuesta) => {
+			.then(async (respuesta) => {
 				const decodedToken = respuesta.data
 				sessionStorage.setItem('token', token)
 				sessionStorage.setItem('user', JSON.stringify(decodedToken.payload[0]))
 				setAuthToken(sessionStorage.getItem('token'))
-				setUser(JSON.parse(sessionStorage.getItem('user')))
+				await setUser(JSON.parse(sessionStorage.getItem('user')))
 				setIsAuth(true)
 			})
 			.catch((error) => {
@@ -46,7 +63,7 @@ export function AuthProvider({ children }) {
 
 	const verifyToken = async (token) => {
 		try {
-			const response = await axios.get(`${urlApi}/api/login/verify`, {
+			const response = await instance.get(`${urlApi}/api/login/verify`, {
 				headers: {
 					Authorization: `Bearer ${token}`,
 				},
@@ -58,28 +75,30 @@ export function AuthProvider({ children }) {
 			setUser(JSON.parse(sessionStorage.getItem('user')))
 			setIsAuth(true)
 		} catch (error) {
-			console.error('Error verificando el token', error)
 			logout()
+			console.error('Error verificando el token', error)
 		}
 	}
 
 	const initialState = {
 		cart: {
-			cartItems: ['test'],
+			cartItems: [{ prodName: 'galletas', prodValVen: 10000 }],
 		},
 	}
 
 	function reducer(state, action) {
 		switch (action.type) {
 			case 'CART_ADD_ITEM':
-				//Cuando se agrega un producto al carrito de compras.
 				const newItem = action.payload
 				const existItem = state.cart.cartItems.find((item) => item._id === newItem._id)
 				const cartItems = existItem
 					? state.cart.cartItems.map((item) => (item._id === existItem._id ? newItem : item))
 					: [...state.cart.cartItems, newItem]
 				return { ...state, cart: { ...state.cart, cartItems } }
-
+			case 'CART_DEL_ITEM':
+				const payloadItem = action.payload
+				const newCartItems = state.cart.cartItems.filter((item) => item.prodId !== payloadItem.prodId)
+				return { ...state, cart: { ...state.cart, cartItems: newCartItems } }
 			default:
 				return state
 		}
@@ -106,8 +125,6 @@ export function AuthProvider({ children }) {
 				login,
 				logout,
 				urlApi,
-				cartItems,
-				setCartItems,
 				state,
 				dispatch,
 			}}>
--- src/components/operaciones/Operaciones.jsx
+++ src/components/operaciones/Operaciones.jsx
@@ -1,26 +1,57 @@
 /** @format */
 
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 import './operaciones.css'
 
 const Operaciones = () => {
+
+	function reducer (state, action){
+		
+	}
 	const [tipoTrs, setTipoTrs] = useState(7)
+	const [transacData, setTransacData] = useState({
+		
+	})
+
+	function inputHandler(event) {
+		setTransacData({
+			...transacData,
+			[event.target.name]: event.target.value,
+		})
+	}
+
 	function formHandler(event) {}
 
 	return (
 		<>
 			<section className={'w-full md:w-10/12 m-auto'}>
-				<div className='flex w-full gap-10 align-items-end card-head'>
-					<button className={`cardTab btn-ven ${tipoTrs === 7 && "activeTab"}`} onClick={() => setTipoTrs(7)}>
+				<div className={`card-head ${tipoTrs === 7 ? 'pr-3' : 'pl-3'}`}>
+					<button className={`cardTab btn-ven ${tipoTrs === 7 && 'activeTab'}`} onClick={() => setTipoTrs(7)}>
 						Venta
 					</button>
-					<button className={`cardTab btn-rec ${tipoTrs === 6 && "activeTab"}`} onClick={() => setTipoTrs(6)}>
+					<button className={`cardTab btn-rec ${tipoTrs === 6 && 'activeTab'}`} onClick={() => setTipoTrs(6)}>
 						Recarga
 					</button>
 				</div>
-				<div className='card-body'>
+				<div className={`card-body ${tipoTrs === 7 ? 'rounded-tr-lg' : 'rounded-tl-lg'}`}>
 					<form method='POST' className='' onSubmit={formHandler}>
-						<h1>{tipoTrs === 7 ? 'Pagando' : 'Recargando'}</h1>
+						{/* <h1>{tipoTrs === 7 ? 'Pagando' : 'Recargando'}</h1> */}
+						<div className='row'>
+							<div className='w-full md:w-1/2'>
+								<label htmlFor='prodValVen' className='form-label'>
+									Precio de Venta
+								</label>
+								<input
+									type='number'
+									step={'any'}
+									name='prodValVen'
+									id='prodValVen'
+									className='input'
+									onInput={inputHandler}
+									required
+								/>
+							</div>
+						</div>
 					</form>
 				</div>
 			</section>
