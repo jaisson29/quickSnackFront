e50ce9ec78d881fd7ece20dfdcca0b41a85543a4
Your Name <you@example.com>
2024-03-01 12:04:02 -0500

    mejorando usuarioinfo

--- src/pages/usuarioInfo/UsuarioInfo.tsx
+++ src/pages/usuarioInfo/UsuarioInfo.tsx
@@ -10,10 +10,27 @@ const UsuarioInfo = () => {
 	const usuImg = user.usuImg ? `${urlApi}/uploads/${user.usuImg}` : user.usuGen === 1 ? male : female;
 
 	return (
-		<div className={`bg-clBlan`}>
-			<img className={`h-10 w-10 object-cover`} src={usuImg} alt={user.usuNom} />
-			<span>{user.usuNoDoc}</span>
-			{/* <span>{user.usuTipoDoc}</span> */}
+		<div className={`bg-clBlan grid grid-cols-2`}>
+			<div className={`flex justify-center items-center`}>
+				<img className={`h-{70} w-{40} object-cover`} src={usuImg} alt={user.usuNom} />
+			</div>
+			<div>
+				<div>
+					<span>
+						<strong>Nombre:</strong><br />{user.usuNom}
+					</span>
+				</div>
+				<div>
+					<span>
+						<strong>E-mail:</strong><br />{user.usuEmail}
+					</span>
+				</div>
+				<div>
+					<span> 
+						<strong> No. Documento: </strong><br /> {user.usuNoDoc}
+					</span>
+				</div>
+			</div>
 		</div>
 	);
 };
